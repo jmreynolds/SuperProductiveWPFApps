b615f527af600a18526c1ae19e0f4b7b4db4b2fd
Joe Reynolds <josephr@intentdriven.com>
2013-03-04 17:17:22 -0600

    Initial Commit

---
+++ Resources/ResourcesExample/Cascade.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace ResourcesExample
+{
+    public class Cascade : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (UIElement child in Children)
+                child.Measure(new Size(10000, 10000));
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            double top = 0;
+            double left = 0;
+            foreach (UIElement child in Children)
+            {
+                child.Arrange(new Rect(left, top, child.DesiredSize.Width, child.DesiredSize.Height));
+                top += child.DesiredSize.Height;
+                left += 25;
+            }
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
---
+++ Resources/ResourcesExample/MetroExample.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ResourcesExample
+{
+	/// <summary>
+	/// Interaction logic for MetroExample.xaml
+	/// </summary>
+	public partial class MetroExample : Window
+	{
+		public MetroExample()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			MessageBox.Show("Hello World!");
+		}
+	}
+}
